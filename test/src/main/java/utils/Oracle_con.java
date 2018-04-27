8e7598ccec4fcf5f3f8e49887b6a19ae664ab087
caoyg <caoyange0412@163.com>
2018-04-27 13:52:21 +0800

    更新

--- test/src/main/java/utils/Oracle_con.java
+++ test/src/main/java/utils/Oracle_con.java
@@ -8,7 +8,9 @@ public class Oracle_con {
 	
 	public static void main(String[] args) {
 		try {
-			System.out.println(getConn());
+			//System.out.println(getConn());
+			String ss="1.项目学习过程中，设置有任务点，请认真完成各项任务点； 2.为保证学习质量，请按照课程顺序学习，不可跳过、快进。";
+			System.out.println(ss.indexOf("；"));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -16,10 +18,10 @@ public class Oracle_con {
 	}
 	public static Connection getConn() throws Exception{
 		
-		 String URL="jdbc:oracle:thin:@192.168.1.78:1521:orcl";
-		 String USER="tbszyy";
-		 String PASSWORD="tbszyy";
-		 Class.forName("oracle.jdbc.driver.OracleDriver");
+		 String URL="jdbc:mysql://localhost:3306/cpd_test2";
+		 String USER="root";
+		 String PASSWORD="12345678";
+		 Class.forName("com.mysql.jdbc.Driver");
 		 Connection conn=DriverManager.getConnection(URL, USER, PASSWORD);
 		 return conn;
 	}
