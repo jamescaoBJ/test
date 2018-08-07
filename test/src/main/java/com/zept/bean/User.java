a1278bbcd1281c5f3355d5d845c132b5468d4a65
caoyg <caoyange0412@163.com>
2018-08-07 14:46:08 +0800

    dayli update

--- test/src/main/java/com/zept/bean/User.java
+++ test/src/main/java/com/zept/bean/User.java
@@ -1,10 +1,14 @@
 package com.zept.bean;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
 	
 	private String username;
 	private String password;
 	private int age;
+    private Integer seq;
 	public String getUsername() {
 		return username;
 	}
@@ -25,5 +29,23 @@ public class User {
 	}
 	
 	public static void main(String[] args) {
+		List<String> list =new ArrayList<String>();
+		list.add("123");
+		//System.out.println(list.size());
+		List<String> list2 =new ArrayList<String>();
+		list2.add("000p");
+		list.addAll(list2);
+		System.out.println(list.size());
+		for(String ss:list){
+			System.out.println(ss);
+		}
+		/*User   user=new User();
+		System.out.println(user.getAge());*/
+	}
+	public Integer getSeq() {
+		return seq;
+	}
+	public void setSeq(Integer seq) {
+		this.seq = seq;
 	}
 }
